e2c1f9139a9c18a48c71ee8ad419caed1e90cede
alex-njuguna <njugunakinuthia013@gmail.com>
2024-07-28 19:23:26 +0300

    creates model Profile and administration

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email"]
+    list_display_links = ["id"]
+    search_fields = ["name", "email", "bio"]
+    
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,12 @@
+"""
+Profile - name, email, bio
+"""
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=250)
+    email = models .EmailField(max_length=250, blank=True, null=True)
+    bio = models. TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
