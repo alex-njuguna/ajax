ffbc68173326163b7b0d8c246e8e705dc7aa732d
alex-njuguna <njugunakinuthia013@gmail.com>
2024-07-28 21:10:36 +0300

    display data with ajax

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    
+    path("", views.index, name="name"),
+    path("get_profiles/", views.get_profiles, name="get_profiles"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Profile
+
+
+def index(request):
+    """Home page"""
+    return render(request, "profile/index.xhtml")
+
+def get_profiles(request):
+    """Get all the profiles from the database"""
+    profiles = Profile.objects.all().order_by("id")
+
+    context = {
+        "profiles": list(profiles.values())
+    }
+
+    return JsonResponse(context)
